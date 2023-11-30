1164e686482b4326aa300c030a8b174bc3f35d90
XavNTU <150364573+XavNTU@users.noreply.github.com>
2023-11-30 14:58:20 +0000

    qwe.py

--- qwe.py
+++ qwe.py
@@ -13,3 +13,10 @@ class Flight:
         self.speed = speed
         self.new_time = new_time
 
+   def calculate_estimated_arrival_time(self):
+        if self.current_speed == 0:
+            return "Cannot calculate estimated arrival time with zero speed"
+
+        time_to_arrival = timedelta(hours=self.current_distance / self.current_speed)
+        estimated_arrival_time = datetime.strptime(self.scheduled_arrival_time, "%H:%M") + time_to_arrival
+        return estimated_arrival_time.strftime("%H:%M
